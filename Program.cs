f4d995a5bc0a54aae259067c8e9123268583aced
baha.rashed@cloudsolution-sa.com <baha.rashed@cloudsolution-sa.com>
2023-04-11 19:31:15 +0300

    Add configuration

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
- 
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("CommandConStr")));
+
+builder.Services.AddGraphQLServer()
+                .AddQueryType<Query>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
 
 app.MapGet("/", () => "Hello World!");
 
